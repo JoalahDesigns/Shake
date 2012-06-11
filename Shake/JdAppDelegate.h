629d250636b1de2018752c5c363e2187e75845e0
Peter Milway <peter@JoalahDesigns.com>
2012-06-11 18:00:16 -0400

    Initial Commit

---
+++ Shake/JdAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JdAppDelegate.h
+//  Shake
+//
+//  Created by Peter Milway on 6/11/12.
+//  Copyright (c) 2012 Joalah Designs LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JdAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
